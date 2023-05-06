49e66f8dab64fb8ffd709ceefdc74da7e49770f7
Haafii <hafismuhammedtk@gmail.com>
2023-05-06 23:42:04 +0530

    profile page added

--- rubbish-revolution/src/App.jsx
+++ rubbish-revolution/src/App.jsx
@@ -10,15 +10,17 @@ import {
 import Home from './pages/Home';
 import Header from './components/Header';
 import Scanner from './pages/Scanner';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/scanner" element={<Scanner/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/scanner" element={<Scanner />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Footer />
       </Router>
--- rubbish-revolution/src/pages/Home.jsx
+++ rubbish-revolution/src/pages/Home.jsx
@@ -8,17 +8,19 @@ function Home() {
       <section className="bg-primary text-white body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">PLASTIC WASTE 
-              <br className="hidden lg:inline-block"/> MANAGER
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">PLASTIC WASTE
+              <br className="hidden lg:inline-block" /> MANAGER
             </h1>
             <p className="mb-8 leading-relaxed">In order to create a plastic free environment, there is a need to establish a system where people can enjoy doing the necessity of Going GREEN. We are here to bring about the change needed.</p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600  rounded text-lg">Get started</button>
+              <a href="profile">
+                <button className="inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600  rounded text-lg">Get started</button>
+              </a>
               <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">Learn more</button>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded-xl" alt="hero" src="/images/hero_img.jpg"/>
+            <img className="object-cover object-center rounded-xl" alt="hero" src="/images/hero_img.jpg" />
           </div>
         </div>
       </section>
