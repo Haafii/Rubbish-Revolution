6fba30db89c8230f07caf4cf1106909fe33802b0
Haafii <hafismuhammedtk@gmail.com>
2023-05-16 23:56:52 +0530

    log in page changes

--- rubbish-revolution/src/pages/Login.jsx
+++ rubbish-revolution/src/pages/Login.jsx
@@ -74,9 +74,9 @@ const Login = () => {
   
   const LoginForm = () => {
     return(
-       <div className="bg-white rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-out">
-             <h2 className='p-3 text-3xl font-bold text-pink-400'>Rubbish Revolution</h2>
-             <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
+       <div className="bg-primary rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-out">
+             <h2 className='p-3 text-3xl font-bold text-blue-400'>Rubbish Revolution</h2>
+             <div className="inline-block border-[1px] justify-center w-20 border-gray-400 border-solid"></div>
              <h3 className='text-xl font-semibold text-blue-400 pt-2'>Welcome Back</h3>
              <div className='flex space-x-2 m-4 items-center justify-center'>
                 <div className="socialIcon">
@@ -91,23 +91,23 @@ const Login = () => {
              </div>
              {/* Inputs */}
              <div className='flex flex-col items-center justify-center'>
-              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Email'></input>
-              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Password'></input>
-              <button className='rounded-2xl m-2 text-white bg-blue-400 w-2/5 px-4 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in'>
+              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Email'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Password'></input>
+              <button className='rounded-2xl m-2 text-white bg-sky-500 w-2/5 px-4 py-2 shadow-md hover:text-secondary hover:bg-sky-600 transition duration-200 ease-in'>
                 Sign In
               </button>
              </div>
-             <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
-             <p className='text-blue-400 mt-4 text-sm'>Don't have an account?</p>
-             <p className='text-blue-400 mb-4 text-sm font-medium cursor-pointer' onClick={() => setIsLogin(false)}>Create a New Account?</p>
+             <div className="inline-block border-[1px] justify-center w-20 border-gray-400 border-solid"></div>
+             <p className='text-white mt-4 text-sm'>Don't have an account?</p>
+             <p className='text-white mb-4 text-sm font-medium cursor-pointer' onClick={() => setIsLogin(false)}>Create a New Account?</p>
           </div>
     )
   }
   
   const  SignUpForm = () => {
      return(
-        <div className="bg-blue-400 text-black rounded-2xl shadow-2xl  flex flex-col w-full  md:w-1/3 items-center max-w-4xl transition duration-1000 ease-in">
-              <h2 className='p-3 text-3xl font-bold text-white'>Horiz</h2>
+        <div className="bg-primary text-black rounded-2xl shadow-2xl  flex flex-col w-full  md:w-1/3 items-center max-w-4xl transition duration-1000 ease-in">
+              <h2 className='p-3 text-3xl font-bold text-white'>Rubbish Revolution</h2>
              <div className="inline-block border-[1px] justify-center w-20 border-white border-solid"></div>
              <h3 className='text-xl font-semibold text-white pt-2'>Create Account!</h3>
              <div className='flex space-x-2 m-4 items-center justify-center'>
@@ -123,11 +123,11 @@ const Login = () => {
              </div>
              {/* Inputs */}
              <div className='flex flex-col items-center justify-center mt-2'>
-             <input type='text' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Name'></input>
-              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Email'></input>
-              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Password'></input>
-              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Avatar URL'></input>
-              <button className='rounded-2xl m-4 text-blue-400 bg-white w-3/5 px-4 py-2 shadow-md hover:text-white hover:bg-blue-400 transition duration-200 ease-in'>
+             <input type='text' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Name'></input>
+              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Email'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Password'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px]  m-1 focus:shadow-md  focus:outline-none focus:ring-0' placeholder='Confirm Password'></input>
+              <button className='rounded-2xl m-4 text-white bg-sky-500 w-3/5 px-4 py-2 shadow-md hover:text-secondary hover:bg-sky-600 transition duration-200 ease-in'>
                 Sign Up
               </button>
              </div>
@@ -139,11 +139,11 @@ const Login = () => {
   }
 
   return (
-    <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen md:py-2">
+    <div className="bg-gray-600 flex flex-col items-center justify-center min-h-screen md:py-2">
     <main className="flex items-center w-full px-2 md:px-20">
       <div className="hidden md:inline-flex flex-col flex-1 space-y-1">
-        <p className='text-6xl text-blue-500 font-bold'>Horiz</p>
-        <p className='font-medium text-lg leading-1 text-pink-400'>Explore your interests, meet new friends & expand your horions</p>
+        <p className='text-6xl text-blue-500 font-bold'>Rubbish Revolution</p>
+        <p className='font-medium text-lg leading-1 text-primary'>Stop the pollution. Be part of the solution.</p>
       </div>
       {
         isLogin? (
