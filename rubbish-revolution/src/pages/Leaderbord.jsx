a220e9568cbc23c82feb5b89145ed64821e0af87
Haafii <hafismuhammedtk@gmail.com>
2023-05-18 23:07:49 +0530

    ldr board updated

--- rubbish-revolution/src/pages/Leaderbord.jsx
+++ rubbish-revolution/src/pages/Leaderbord.jsx
@@ -7,7 +7,7 @@ const Leaderboard = () => {
       <Header />
       <div className='bg-primary'>
         <div className='bg-secondary '>
-          ldrkk
+          leader Board
         </div>
       </div>
     </div>
