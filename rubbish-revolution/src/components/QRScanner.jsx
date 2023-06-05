ec444f6e1c8b3d8237ba7eb8a6f776e7eda7e4d5
Haafii <hafismuhammedtk@gmail.com>
2023-06-05 22:44:59 +0530

    qrbackednd set

--- rubbish-revolution/src/components/QRScanner.jsx
+++ rubbish-revolution/src/components/QRScanner.jsx
@@ -35,6 +35,8 @@ const QRScanner = () => {
   const [qrData, setQRData] = useState('');
   const [scanned, setScanned] = useState(false);
   const navigate = useNavigate();
+  let productQr = []
+  let userQr = []
 
   const handleScan = (data) => {
     if (data) {
@@ -51,8 +53,10 @@ const QRScanner = () => {
     if (scanned) {
       // Redirect to the profile page
       if(qrData.includes("rubbishrevolution")){
+        userQr.push(qrData)
         navigate('/scoreincreased');
       } else {
+        productQr.push(qrData)
         navigate('/scanagain')
       }
     }
@@ -68,7 +72,9 @@ const QRScanner = () => {
           style={{ width: '100%' }}
         />
       )}
-      {console.log(qrData)}
+      {/* {console.log(qrData)} */}
+      {console.log("products" , productQr)}
+      {console.log("user qr ",userQr)}
     </div>
   );
 };
@@ -76,4 +82,63 @@ const QRScanner = () => {
 export default QRScanner;
 
 
+// import React, { useState, useEffect } from 'react';
+// import { QrReader } from 'react-qr-reader';
+// import { useNavigate } from 'react-router-dom';
 
+// const QRScanner = () => {
+//   const [qrData, setQRData] = useState('');
+//   const [scanned, setScanned] = useState(false);
+//   const [camera, setCamera] = useState('user'); // Add this state variable
+//   const navigate = useNavigate();
+
+//   const handleScan = (data) => {
+//     if (data) {
+//       setQRData(data.text);
+//       setScanned(true);
+//     }
+//   };
+
+//   const handleError = (error) => {
+//     console.error(error);
+//   };
+
+//   const toggleCamera = () => { // Add this function
+//     setCamera((prevCamera) => (prevCamera === 'user' ? 'environment' : 'user'));
+//   };
+
+//   useEffect(() => {
+//     if (scanned) {
+//       if (qrData.includes("rubbishrevolution")) {
+//         navigate('/scoreincreased');
+//       } else {
+//         navigate('/scanagain');
+//       }
+//     }
+//   }, [scanned, navigate]);
+
+//   return (
+//     <div>
+//       {!scanned && (
+//         <>
+//           <QrReader
+//             delay={300}
+//             onError={handleError}
+//             onResult={handleScan}
+//             facingMode={camera} // Add this prop
+//             style={{ width: '100%' }}
+//           />
+//           <div className='flex items-center justify-center '>
+//             <button
+//               className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//               onClick={toggleCamera}> Switch Camera
+//             </button>
+//           </div>
+//         </>
+//       )}
+//       {console.log(qrData)}
+//     </div>
+//   );
+// };
+
+// export default QRScanner;
