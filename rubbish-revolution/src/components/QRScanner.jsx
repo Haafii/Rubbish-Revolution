6d17056c2cc2d218cfc38e3c46e30a5ad2b93e21
Haafii <hafismuhammedtk@gmail.com>
2023-06-06 22:32:45 +0530

    barcode scanner  added

--- rubbish-revolution/src/components/QRScanner.jsx
+++ rubbish-revolution/src/components/QRScanner.jsx
@@ -1,5 +1,3 @@
-// import React, { useRef, useState } from 'react';
-// import { BrowserBarcodeReader } from '@zxing/library';
 
 // const QRScanner = () => {
 //   const videoRef = useRef(null);
@@ -26,121 +24,20 @@
 
 // export default QRScanner;
 
-
-import React, { useState, useEffect, useRef } from 'react';
-import { QrReader } from 'react-qr-reader';
-import { useNavigate } from 'react-router-dom';
-
-const QRScanner = () => {
-  const [qrData, setQRData] = useState('');
-  const [scanned, setScanned] = useState(false);
-  const [status, setStatus] = useState("");
-  const navigate = useNavigate();
-  // let productQr = []
-  // let userQr = []
-  const role = localStorage.getItem('role');
-  let productQr = JSON.parse(localStorage.getItem('productQr')) || [];
-  let userQr = JSON.parse(localStorage.getItem('userQr')) || [];
-  let garbageQr = JSON.parse(localStorage.getItem('garbageQr')) || [];
-
-
-
-  const handleScan = (data) => {
-    if (data) {
-      setQRData(data.text);
-      setScanned(true);
-    }
-  };
-  const handleError = (error) => {
-    console.error(error);
-  };
-
-  useEffect(() => {
-    if (scanned) {
-      // Redirect to the profile page
-      if (qrData.includes("rubbishrevolutionuser")) {
-        userQr.push(qrData)
-        localStorage.setItem('userQr', JSON.stringify(userQr));
-        navigate('/scoreincreased');
-      }
-      else if (qrData.includes("rubbishrevolutionstore")) {
-        navigate('/transaction');
-      } else if (qrData.includes("rubbishgarbage")) {
-        if (garbageQr.length > 0) {
-          garbageQr.shift(); // Remove the first entry from the array
-        }
-        garbageQr.unshift(qrData); // Add the new value at index 0 of the array
-        localStorage.setItem('garbageQr', JSON.stringify(garbageQr));
-        const url = 'https://mini-project-mkgl.onrender.com/garbage_validation';
-        const payload = {
-          gid: garbageQr[0]
-        };
-
-        const options = {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            Authorization: `Bearer ${localStorage.getItem('userId')}`,
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(payload)
-        };
-
-        fetch(url, options)
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            setStatus(data.status)
-            localStorage.setItem('status', data.status);
-            navigate("/garbagecheck")
-            // Process the response data here
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            // Handle any errors that occurred during the request
-          });
-      }
-
-      else {
-        productQr.push(qrData)
-        localStorage.setItem('productQr', JSON.stringify(productQr));
-        navigate('/scanagain')
-      }
-    }
-  }, [scanned, navigate]);
-
-  return (
-    <div>
-      {!scanned && (
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onResult={handleScan}
-          style={{ width: '100%' }}
-        />
-      )}
-      {/* {console.log(qrData)} */}
-      {console.log("products", productQr)}
-      {console.log("user qr ", userQr)}
-      {console.log("garbage Qr ", garbageQr)}
-      {console.log(status)}
-
-    </div>
-  );
-};
-
-export default QRScanner;
-
-
-// import React, { useState, useEffect } from 'react';
+// import { BrowserBarcodeReader } from '@zxing/library';
+// import React, { useState, useEffect, useRef } from 'react';
 // import { QrReader } from 'react-qr-reader';
 // import { useNavigate } from 'react-router-dom';
 
 // const QRScanner = () => {
 //   const [qrData, setQRData] = useState('');
 //   const [scanned, setScanned] = useState(false);
-//   const [camera, setCamera] = useState('user'); // Add this state variable
+//   const [status, setStatus] = useState("");
 //   const navigate = useNavigate();
+//   const role = localStorage.getItem('role');
+//   let productQr = JSON.parse(localStorage.getItem('productQr')) || [];
+//   let userQr = JSON.parse(localStorage.getItem('userQr')) || [];
+//   let garbageQr = JSON.parse(localStorage.getItem('garbageQr')) || [];
 
 //   const handleScan = (data) => {
 //     if (data) {
@@ -148,21 +45,60 @@ export default QRScanner;
 //       setScanned(true);
 //     }
 //   };
-
 //   const handleError = (error) => {
 //     console.error(error);
 //   };
 
-//   const toggleCamera = () => { // Add this function
-//     setCamera((prevCamera) => (prevCamera === 'user' ? 'environment' : 'user'));
-//   };
-
 //   useEffect(() => {
 //     if (scanned) {
-//       if (qrData.includes("rubbishrevolution")) {
+//       // Redirect to the profile page
+//       if (qrData.includes("rubbishrevolutionuser")) {
+//         userQr.push(qrData)
+//         localStorage.setItem('userQr', JSON.stringify(userQr));
 //         navigate('/scoreincreased');
-//       } else {
-//         navigate('/scanagain');
+//       }
+//       else if (qrData.includes("rubbishrevolutionstore")) {
+//         navigate('/transaction');
+//       } else if (qrData.includes("rubbishgarbage")) {
+//         if (garbageQr.length > 0) {
+//           garbageQr.shift(); // Remove the first entry from the array
+//         }
+//         garbageQr.unshift(qrData); // Add the new value at index 0 of the array
+//         localStorage.setItem('garbageQr', JSON.stringify(garbageQr));
+//         const url = 'https://mini-project-mkgl.onrender.com/garbage_validation';
+//         const payload = {
+//           gid: garbageQr[0]
+//         };
+
+//         const options = {
+//           method: 'POST',
+//           headers: {
+//             'Accept': 'application/json',
+//             Authorization: `Bearer ${localStorage.getItem('userId')}`,
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify(payload)
+//         };
+
+//         fetch(url, options)
+//           .then(response => response.json())
+//           .then(data => {
+//             console.log(data);
+//             setStatus(data.status)
+//             localStorage.setItem('status', data.status);
+//             navigate("/garbagecheck")
+//             // Process the response data here
+//           })
+//           .catch(error => {
+//             console.error('Error:', error);
+//             // Handle any errors that occurred during the request
+//           });
+//       }
+
+//       else {
+//         productQr.push(qrData)
+//         localStorage.setItem('productQr', JSON.stringify(productQr));
+//         navigate('/scanagain')
 //       }
 //     }
 //   }, [scanned, navigate]);
@@ -170,25 +106,150 @@ export default QRScanner;
 //   return (
 //     <div>
 //       {!scanned && (
-//         <>
-//           <QrReader
-//             delay={300}
-//             onError={handleError}
-//             onResult={handleScan}
-//             facingMode={camera} // Add this prop
-//             style={{ width: '100%' }}
-//           />
-//           <div className='flex items-center justify-center '>
-//             <button
-//               className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-//               onClick={toggleCamera}> Switch Camera
-//             </button>
-//           </div>
-//         </>
+//         <QrReader
+//           delay={300}
+//           onError={handleError}
+//           onResult={handleScan}
+//           style={{ width: '100%' }}
+//         />
 //       )}
-//       {console.log(qrData)}
+//       {/* {console.log(qrData)} */}
+//       {console.log("products", productQr)}
+//       {console.log("user qr ", userQr)}
+//       {console.log("garbage Qr ", garbageQr)}
+//       {console.log(status)}
+
 //     </div>
 //   );
 // };
 
 // export default QRScanner;
+
+import { BrowserBarcodeReader } from '@zxing/library';
+import React, { useState, useEffect, useRef } from 'react';
+import { QrReader } from 'react-qr-reader';
+import { useNavigate } from 'react-router-dom';
+
+const QRScanner = () => {
+  const [qrData, setQRData] = useState('');
+  const [scanned, setScanned] = useState(false);
+  const [status, setStatus] = useState('');
+  const navigate = useNavigate();
+  const role = localStorage.getItem('role');
+  let productQr = JSON.parse(localStorage.getItem('productQr')) || [];
+  let userQr = JSON.parse(localStorage.getItem('userQr')) || [];
+  let garbageQr = JSON.parse(localStorage.getItem('garbageQr')) || [];
+
+  const videoRef = useRef(null); // Define videoRef using useRef
+
+  const handleScan = (data) => {
+    if (data) {
+      setQRData(data.text);
+      setScanned(true); 
+    }
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+  };
+
+  useEffect(() => {
+    if (scanned) {
+      // Redirect to the profile page
+      if (qrData.includes('rubbishrevolutionuser')) {
+        userQr.push(qrData);
+        localStorage.setItem('userQr', JSON.stringify(userQr));
+        navigate('/scoreincreased');
+      } else if (qrData.includes('rubbishrevolutionstore')) {
+        navigate('/transaction');
+      } else if (qrData.includes('rubbishgarbage')) {
+        if (garbageQr.length > 0) {
+          garbageQr.shift(); // Remove the first entry from the array
+        }
+        garbageQr.unshift(qrData); // Add the new value at index 0 of the array
+        localStorage.setItem('garbageQr', JSON.stringify(garbageQr));
+        const url = 'https://mini-project-mkgl.onrender.com/garbage_validation';
+        const payload = {
+          gid: garbageQr[0],
+        };
+
+        const options = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('userId')}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        };
+
+        fetch(url, options)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            setStatus(data.status);
+            localStorage.setItem('status', data.status);
+            navigate('/garbagecheck');
+            // Process the response data here
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            // Handle any errors that occurred during the request
+          });
+      } else {
+        productQr.push(qrData);
+        localStorage.setItem('productQr', JSON.stringify(productQr));
+        navigate('/scanagain');
+      }
+    }
+  }, [scanned, navigate]);
+
+  useEffect(() => {
+    const codeReader = new BrowserBarcodeReader();
+
+    const startBarcodeScanner = async () => {
+      try {
+        const devices = await codeReader.getVideoInputDevices();
+        const selectedDeviceId = devices[0].deviceId;
+        const videoElement = videoRef.current;
+
+        if (videoElement) {
+          codeReader.decodeFromVideoDevice(selectedDeviceId, videoElement, (result, error) => {
+            if (result) {
+              setQRData(result.text);
+              setScanned(true);
+            } else if (error) {
+              // console.error('Barcode scanning error:', error);
+            }
+          });
+        }
+      } catch (error) {
+        console.error('Error starting barcode scanner:', error);
+      }
+    };
+
+    startBarcodeScanner();
+
+    return () => {
+      codeReader.reset();
+    };
+  }, []);
+
+  return (
+    <div>
+      {!scanned && (
+        <>
+          <QrReader delay={300} onError={handleError} onResult={handleScan} style={{ width: '100%' }} />
+          <video ref={videoRef} style={{ display: 'none' }}></video>
+        </>
+      )}
+      {console.log('qrData:', qrData)}
+      {console.log('products:', productQr)}
+      {console.log('user qr:', userQr)}
+      {console.log('garbage Qr:', garbageQr)}
+      {console.log('status:', status)}
+    </div>
+  );
+};
+
+export default QRScanner;
