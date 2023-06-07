5da2369c4414d562aec907d5770272cc503e425b
Haafii <hafismuhammedtk@gmail.com>
2023-06-07 20:58:08 +0530

    admin control button added

--- rubbish-revolution/src/pages/Home.jsx
+++ rubbish-revolution/src/pages/Home.jsx
@@ -55,14 +55,16 @@ function Home() {
                   <button className="inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600  rounded text-lg">Add Product</button>
                 </Link>
               }
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">Learn more</button>
-              {role == "admin" ?
-                <Link to={"/admin/register"}>
-                  <button className="ml-4 inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600  rounded text-lg">Add new</button>
-                </Link>
-                : ""
-              }
-
+              {role === "admin" && (
+                <React.Fragment>
+                  <Link to="">
+                    <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">Control</button>
+                  </Link>
+                  <Link to="/admin/register">
+                    <button className="ml-4 inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600 rounded text-lg">Add new</button>
+                  </Link>
+                </React.Fragment>
+              )}
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
