c3e666c4d4b02f7b4ee85f79efe29d522a2f1449
Haafii <hafismuhammedtk@gmail.com>
2023-06-13 22:24:43 +0530

    admin control header and point aded

--- rubbish-revolution/src/pages/AdminControl.jsx
+++ rubbish-revolution/src/pages/AdminControl.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Header from "../components/Header";
 
 function AdminControl() {
   const buttonValue = localStorage.getItem('admincontrolbuttonvalue');
@@ -76,7 +77,7 @@ function AdminControl() {
     console.log("handle edit " + playerName);
   }
   function handleAddPoints() {
-    
+
     console.log('Adding points:', earnedPoints);
     console.log("handle add point " + playerName);
     // Add your logic to handle the added points here
@@ -135,105 +136,111 @@ function AdminControl() {
   }
 
   return (
-    <div className="mx-auto h-screen bg-primary">
-      {buttonValue === 'delete' && (
-        <>
-          <div className="bg-red-500 text-white py-4 px-8 flex justify-center">
-            Delete Accounts
-          </div>
-          {leaderboardData.map((player, index) => (
-            <div
-              key={index}
-              className={`flex items-center px-6 py-4 bg-secondary rounded-md mb-2 `}
-            >
-              <img
-                className="h-10 w-10 rounded-full mr-4 object-cover"
-                src={player.avatar}
-                alt={player.name}
-              />
-              <div className="text-lg font-semibold text-white">
-                {index + 1}. {player.name}
-              </div>
-              {(
-                <button
-                  className="text-lg ml-auto text-white font-bold"
-                  onClick={() => handleDelete(player.name)}
-                >
-                  Delete
-                </button>
-              )}
+    <>
+      <Header />
+      <div className="mx-auto h-screen bg-primary">
+        {buttonValue === 'delete' && (
+          <>
+            <div className="bg-red-500 text-white py-4 px-8 flex justify-center">
+              Delete Accounts
             </div>
-          ))}
-        </>
-      )}
-
-      {buttonValue === 'edit' && (
-        <>
-          <div className="bg-yellow-500 text-white py-4 px-8 flex justify-center">
-            Edit Points
-          </div>
-          {leaderboardData.map((player, index) => (
-            <div
-              key={index}
-              className={`flex items-center px-6 py-4 bg-secondary rounded-md mb-2 `}
-            >
-              <img
-                className="h-10 w-10 rounded-full mr-4 object-cover"
-                src={player.avatar}
-                alt={player.name}
-              />
-              <div className="text-lg font-semibold text-white">
-                {index + 1}. {player.name}
-              </div>
-              {buttonValue === 'edit' && (
-                <button
-                  className="text-lg ml-auto text-white font-bold"
-                  onClick={() => handleEdit(player.name)}
-                >
-                  Edit
-                </button>
-              )}
-            </div>
-          ))}
-        </>
-      )}
-
-      {buttonValue === 'calculate' && (
-        <div className="bg-green-500 text-white py-4 px-8 flex justify-center">
-          Calculate Value
-        </div>
-      )}
-
-      {/* Edit Window */}
-      {isEditWindowOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-8 rounded-md max-w-xs">
-            <h3 className="text-lg font-semibold mb-4">Edit Point</h3>
-            <input
-              type="number"
-              className="px-4 py-2 border border-gray-300 rounded-md w-full mb-4"
-              placeholder="Enter earned points"
-              value={earnedPoints}
-              onChange={(e) => setEarnedPoints(e.target.value)}
-            />
-            <div className="flex justify-end">
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
-                onClick={handleAddPoints}
+            {leaderboardData.map((player, index) => (
+              <div
+                key={index}
+                className={`flex items-center px-6 py-4 bg-secondary rounded-md mb-2 `}
               >
-                Add
-              </button>
-              <button
-                className="bg-gray-500 text-white px-4 py-2 rounded-md"
-                onClick={() => setIsEditWindowOpen(false)}
+                <img
+                  className="h-10 w-10 rounded-full mr-4 object-cover"
+                  src={player.avatar}
+                  alt={player.name}
+                />
+                <div className="text-lg font-semibold text-white">
+                  {index + 1}. {player.name}
+                </div>
+                {(
+                  <button
+                    className="text-lg ml-auto text-white font-bold"
+                    onClick={() => handleDelete(player.name)}
+                  >
+                    Delete
+                  </button>
+                )}
+              </div>
+            ))}
+          </>
+        )}
+
+        {buttonValue === 'edit' && (
+          <>
+            <div className="bg-yellow-500 text-white py-4 px-8 flex  justify-center">
+              Edit Points
+            </div>
+            {leaderboardData.map((player, index) => (
+              <div
+                key={index}
+                className={`flex items-center px-6 py-4 bg-secondary rounded-md mb-2 `}
               >
-                Close
-              </button>
+                <img
+                  className="h-10 w-10 rounded-full mr-4 object-cover"
+                  src={player.avatar}
+                  alt={player.name}
+                />
+                <div className="text-lg font-semibold text-white">
+                  {index + 1}. {player.name}
+                </div>
+                <div className="text-lg  ml-auto text-white font-bold">
+                  {player.score}
+                </div>
+                {buttonValue === 'edit' && (
+                  <button
+                    className="text-lg ml-auto text-white font-bold"
+                    onClick={() => handleEdit(player.name)}
+                  >
+                    Edit
+                  </button>
+                )}
+              </div>
+            ))}
+          </>
+        )}
+
+        {buttonValue === 'calculate' && (
+          <div className="bg-green-500 text-white py-4 px-8 flex justify-center">
+            Calculate Value
+          </div>
+        )}
+
+        {/* Edit Window */}
+        {isEditWindowOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-8 rounded-md max-w-xs">
+              <h3 className="text-lg font-semibold mb-4">Edit Point</h3>
+              <input
+                type="number"
+                className="px-4 py-2 border border-gray-300 rounded-md w-full mb-4"
+                placeholder="Enter earned points"
+                value={earnedPoints}
+                onChange={(e) => setEarnedPoints(e.target.value)}
+              />
+              <div className="flex justify-end">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+                  onClick={handleAddPoints}
+                >
+                  Add
+                </button>
+                <button
+                  className="bg-gray-500 text-white px-4 py-2 rounded-md"
+                  onClick={() => setIsEditWindowOpen(false)}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
