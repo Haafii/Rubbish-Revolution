d81f5c37c9a5f8b7acbc6237e8479563d933d248
Haafii <hafismuhammedtk@gmail.com>
2023-06-23 23:00:28 +0530

    project id changed

--- rubbish-revolution/src/pages/Chat/ChatPage.jsx
+++ rubbish-revolution/src/pages/Chat/ChatPage.jsx
@@ -10,7 +10,7 @@ const ChatsPage = (props) => {
         <h3 className='text-white sm:hidden  text-xl mb-2'>Use Desktop View for search</h3>
         <div className="w-4/5 h-4/5 bg-[#2b2f3c] text-white ">
           <PrettyChatWindow
-            projectId="6e3d657c-5b7d-4c07-b5b9-62047ae7ff4a"
+            projectId="02a97cfe-eafc-47ae-aacf-f9adec99db7f"
             username={props.user.username}
             secret={props.user.secret}
           />
